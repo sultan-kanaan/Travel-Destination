095c3e32b77f64e61ebd340d1b1a38cb1610655c
sultan-kanaan <sultan.kanaan92@gmail.com>
2022-03-02 09:24:13 +0300

    ts16

---
+++ components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+export default function footer () {
+    return (
+      <div className ='footer' >
+          <a href="https://github.com/sultan-kanaan"> contact me</a>
+          <br />
+          <h3> &copy;2022 ASAC with sultan kanaan</h3>
+
+      </div>
+    )
+  }
